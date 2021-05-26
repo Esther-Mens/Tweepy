2901c7e3a407a1cb0dc9aac9489450a3cecd7892
Esther-Mens <66975716+Esther-Mens@users.noreply.github.com>
2021-05-26 15:32:48 +0000

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 # this is for authorization
 
-consumer_key = 'pl13zULs9D5CZvvywVw9Qe9BU'
-consumer_secret ='bWcUYdpqsjDOrgAmUV42nK81uZDi0bZ4C9RDHxDcwyUAAI8KaN'
-access_token = '814433953641824256-JNNefTPysP2UVNpu4aY6Y6qDbnNydLr'
-access_token_secret = '9t1WaQbco5tE8gMMLCPTl2u0AwxVNHCCmVaPZrDePywc0'
+consumer_key = 'consumer_key'
+consumer_secret ='consumer_secret'
+access_token = 'access_token'
+access_token_secret = 'access_token_secret'
